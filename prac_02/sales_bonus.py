d8b6a6b651c4ed4a25266c8cc626a29d9ce12c8c
Gaming PC <sdf@gmail.com>
2024-06-02 20:09:02 +1000

    Refactor prac 1 sales_bonus.py

--- prac_02/sales_bonus.py
+++ prac_02/sales_bonus.py
@@ -3,11 +3,32 @@ Program to calculate and display a user's bonus based on sales.
 If sales are under $1,000, the user gets a 10% bonus.
 If sales are $1,000 or over, the bonus is 15%.
 """
-sales = float(input("Enter sales: $"))
-while sales >= 0:  # pseudocode in prac helped
+
+
+def main():
+    get_and_calculate_sales()
+
+
+def get_and_calculate_sales():
+    sales = get_sales()
+    while sales >= 0:
+        bonus = calculate_bonus(sales)
+        print_bonus(bonus)
+        sales = get_sales()
+
+
+def get_sales():
+    return float(input("Enter sales: $"))
+
+
+def calculate_bonus(sales):
     if sales < 1000:
-        bonus = sales * 0.10
+        return sales * 0.10
     else:
-        bonus = sales * 0.15
+        return sales * 0.15
+
+
+def print_bonus(bonus):
     print("Bonus is $", bonus, sep='')
-    sales = float(input("Enter sales: $"))
+
+main()
