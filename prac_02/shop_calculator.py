7cc8f353c88b4c2a8a2c36b0ebe3e179fd2158a2
Gaming PC <sdf@gmail.com>
2024-06-02 20:13:16 +1000

    Refactor prac 1 shop_calculator.py

--- prac_02/shop_calculator.py
+++ prac_02/shop_calculator.py
@@ -2,14 +2,30 @@
 CP1404 - Practical
 Shop Calculator
 """
-total_price = 0
-number_items = int(input("Number of items: "))
-while number_items < 0:
-    print("Can't be below 0")
+
+
+def main():
+    total_price = 0
+    number_items = get_number_of_items()
+    total_price = get_total_price(number_items, total_price)
+    if total_price > 100:
+        total_price *= 0.9
+    print(f"Total price for {number_items} items is ${total_price:.2f}")
+
+
+def get_total_price(number_items, total_price):
+    for i in range(number_items):
+        price = float(input("Price of item: "))
+        total_price += price
+    return total_price
+
+
+def get_number_of_items():
     number_items = int(input("Number of items: "))
-for i in range(number_items):
-    price = float(input("Price of item: "))
-    total_price += price
-if total_price > 100:
-    total_price *= 0.9
-print(f"Total price for {number_items} items is ${total_price:.2f}")
+    while number_items < 0:
+        print("Can't be below 0")
+        number_items = int(input("Number of items: "))
+    return number_items
+
+
+main()
